59859ace6ebff5ff55fe754e3eef21c11ffd463c
saysh0 <nikitaormandzhan@gmail.com>
2026-09-17 00:36:25 +0200

    admin review added

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from reviews.models import Review
 
 # Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'listing', 'rating', 'created_at')
+    search_fields = ('author__username', 'listing__title', 'text')
+    list_filter = ('rating', 'created_at')
+
+admin.site.register(Review, ReviewAdmin)
